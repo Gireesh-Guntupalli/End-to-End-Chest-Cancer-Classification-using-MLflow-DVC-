38e1d9734250d45235831c8315dfc6441aa95c87
Gireesh-Guntupalli <guntupallygireesh@yahoo.com>
2024-09-08 22:39:46 +0200

    stage_03 model trainer creation. Config, Entity, Component and pipeline created, NOT TRAINING YET.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTraining
 from cnnClassifier.pipeline.stage_02_prepare_base_model import (
     PrepareBaseModelTrainingPipeline,
 )
+from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 
 # logger.info("Welcome to Cnn Classifier")
 
@@ -28,6 +29,18 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     prepare_base_model = PrepareBaseModelTrainingPipeline()
     prepare_base_model.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
