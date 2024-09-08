7e7aee1c0cbccec64fc109b304cf34d29e622b59
Gireesh-Guntupalli <guntupallygireesh@yahoo.com>
2024-09-08 10:47:33 +0200

    Prepare base model - vgg16 downloaded, updated and compiled

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # from src.cnnClassifier import logger
 from cnnClassifier import logger  ##logger is in constructer function of cnnClassifier
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import (
+    PrepareBaseModelTrainingPipeline,
+)
 
 # logger.info("Welcome to Cnn Classifier")
 
@@ -12,6 +15,19 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
     obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare base model"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
